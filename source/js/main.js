e71fc48bdcc04049ab130ef363c38102b36f1678
Denis <feizov88@gmail.com>
2021-09-23 16:03:56 +0300

    Добавлены некоторые стили страницы Информация

--- source/js/main.js
+++ source/js/main.js
@@ -4,6 +4,11 @@ var svgBurger = document.querySelector('.page-nav__svg--burger');
 var svgCross = document.querySelector('.page-nav__svg--cross');
 var productionNav = document.querySelector('.nav-list__item:nth-child(3)');
 var subNavProduction = document.querySelector('.subnav');
+var instructionButton = document.querySelector('.instruction__item-button');
+var soilTreatment = document.querySelector('.soil-treatment');
+var seedTreatment = document.querySelector('.seed-treatment');
+var growingseasonTreatment = document.querySelector('.growingseason-treatment');
+var instructionItems = document.querySelectorAll('.instruction__item');
 
 //var linkPr = document.querySelectorAll('a');
 
@@ -22,4 +27,24 @@ navButton.addEventListener('click', function(evt) {
 
 productionNav.addEventListener('click', function(evt) {
     subNavProduction.classList.toggle('subnav--show');
+});
+
+instructionButton.addEventListener('click', function(evt) {
+    evt.preventDefault();
+    for (let i = 0; i < instructionItems.length; i++) {
+        var element = instructionItems[i];
+        var activeElement =  element.querySelector('.is-active');
+        if (activeElement = instructionItems[0]) {
+            activeElement.classList.remove('is-active');
+            instructionItems[1].classList.add('is-active');
+        } else if (activeElement = instructionItems[1]) {
+            activeElement.classList.remove('is-active');
+            instructionItems[2].classList.add('is-active');
+        } else if (activeElement = instructionItems[2]) {
+            activeElement.classList.remove('is-active');
+            instructionItems[0].classList.add('is-active');
+        } else {
+            activeElement = instructionItems[0];
+        }
+    }
 })
