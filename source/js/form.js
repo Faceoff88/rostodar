cf63bc6132dca0e6839044700b730f979e335ba7
Denis <feizov88@gmail.com>
2021-09-28 15:35:21 +0300

    Добавлены полсностью стили страницы Контакты и формы

---
+++ source/js/form.js
@@ -0,0 +1,28 @@
+var modalFailure = document.querySelector('.modal-failure');
+var modalSucces = document.querySelector('.modal-succes');
+var modalSuccesButton = modalSucces.querySelector('.modal-succes__button');
+var consultationForm = document.querySelector('.consultation__form');
+var credentialsLabel = consultationForm.querySelectorAll('.credentials__data');
+var formSubmit = consultationForm.querySelector('.consultation__submit');
+var modalFailureButton = modalFailure.querySelector('.modal-failure__button');
+
+
+formSubmit.addEventListener('click', function(evt) {
+  evt.preventDefault();
+  for (let i = 0; i < credentialsLabel.length; i++) {
+    const element = credentialsLabel[i];
+    if (element.value == "") {
+      modalFailure.classList.add('modal-failure--show');
+    } else {
+      modalSucces.classList.add('modal-succes--show');
+    }
+  }
+})
+
+modalFailureButton.addEventListener('click', function(evt) {
+  modalFailure.classList.remove('modal-failure--show');
+})
+
+modalSuccesButton.addEventListener('click', function (evt) {
+  modalSucces.classList.remove('modal-succes--show');
+})
